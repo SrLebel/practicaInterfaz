1b193a661e4a27f3b71cb5cd56c0a07a489e3dbe
SrLebel <juansimancas2@gmail.com>
2022-11-07 13:54:48 -0500

    e

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -4,24 +4,30 @@ import java.awt.*;
 public class menuPrincipal {
     static Color base = new Color(36, 39, 58);
     static Color pink = new Color(245, 189, 230);
+    static Color text = new Color(202, 211, 245);
 
     void generarComponentes () {
         JFrame interfazPrincipal = new JFrame();
         JPanel panelPrincipal = new JPanel();
+        JComboBox menu = new JComboBox();
+        JButton Ingreso = new JButton("Ingresar");
         JLabel texto = new JLabel("Menu Principal \n Open Academic");
 
-        interfazPrincipal.setSize(600, 400);
+        interfazPrincipal.setSize(1440, 1024);
         interfazPrincipal.setVisible(true);
         interfazPrincipal.setLayout(null);
         
-        panelPrincipal.setSize(600, 400);
+        panelPrincipal.setSize(1440, 1024);
         panelPrincipal.setVisible(true);
         panelPrincipal.setBackground(base);
+
+        menu.setVisible(true);
         
-        texto.setForeground(pink);
-        
+        texto.setForeground(text);
+        texto.setBounds(50, 100, 100, 30);
+
         interfazPrincipal.add(panelPrincipal);
         panelPrincipal.add(texto);
-
+        panelPrincipal.add(menu);
     }
 }
